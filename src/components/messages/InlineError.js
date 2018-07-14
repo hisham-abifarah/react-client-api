3d303c243b49ae89cb97baca6c47de8cb3f97423
Hisham Abi Farah <hisham.abi.farah@gmail.com>
2018-07-14 15:40:40 +0300

    Login with client side validation

---
+++ src/components/messages/InlineError.js
@@ -0,0 +1,17 @@
+// rscp + enter
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InlineError = ({ text }) => (
+        <span style = {{ color: '#ae5856' }}>
+        {/* {text} is a proptype have to install prop-types  */}
+            { text }            
+        </span>
+    );
+
+ // proptypes def
+InlineError.propTypes = {
+    text: PropTypes.string.isRequired
+};
+
+export default InlineError;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,34 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
-    <div>
-        <h1>Login Page </h1>
-    </div>
-);
+ // page will render LoginForm
+ // loginform take some function, guard date from form and pass data into this function
+ // form doesnt know what will happen with data, it will pass it along only
 
+
+ // const LoginPage = () => (
+//     <div className ="ui container">
+//         <h1>Login Page </h1>
+
+//         <LoginForm submit = {this.submit}/>
+//     </div>
+// );
+
+class LoginPage extends React.Component{
+
+    submit = (data) =>{
+        console.log(data);
+    };
+
+    render(){
+        return(
+            <div className="ui container">
+                <h1>Login Page </h1>
+
+                <LoginForm submit={this.submit} />
+            </div>
+        );
+    }
+}
 
 export default LoginPage;
